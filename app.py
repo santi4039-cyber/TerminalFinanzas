dab242c779b21450f3d53075005da05af0ff5b3e
santi4039-cyber <santi403940@gmail.com>
2026-05-04 14:20:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import socket
 import os
-
-# IMPORTAMOS NUESTROS MÓDULOS
 import auth as auth
 import Finance_IA as fin
 
@@ -12,18 +10,16 @@ import Finance_IA as fin
 st.set_page_config(page_title="PredicAI Terminal", layout="wide")
 if "GEMINI_API_KEY" in st.secrets:
     API_KEY = st.secrets["GEMINI_API_KEY"]
-else:
-    # Esto es por si lo corrés en tu PC y tenés una variable de entorno
+else:    
     API_KEY = os.getenv("GEMINI_API_KEY", "")
 
-# Validamos que la clave exista antes de configurar
 if API_KEY:
     model = fin.configurar_ia(API_KEY)
 else:
     st.error("Falta la API Key. Configurala en los Secrets de Streamlit.")
 
 
-# --- SIDEBAR: LOGIN Y REGISTRO ---
+# LOGIN Y REGISTRO
 auth.conectar_db()
 with st.sidebar:
     st.title("👤 Usuario")
@@ -45,7 +41,7 @@ with st.sidebar:
             st.session_state['logged_in'] = False
             st.rerun()
 
-# --- DASHBOARD ---
+# DASHBOARD
 st.title("📊 PredicAI: Nasdaq 100 Terminal")
 tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA"]
 ticker = st.selectbox("Activo:", tickers)
@@ -53,21 +49,21 @@ ticker = st.selectbox("Activo:", tickers)
 if ticker:
     info, stock_obj = fin.obtener_datos(ticker)
     
-    # --- FILA 1: MÉTRICAS DE VALORACIÓN ---
+    #MÉTRICAS DE VALORACIÓN
     c1, c2, c3, c4 = st.columns(4)
     c1.metric("Precio Actual", f"${info.get('currentPrice', 'N/A')}")
     c2.metric("Market Cap", f"{info.get('marketCap', 0)/1e9:.2f}B")
     c3.metric("P/E Ratio", info.get('trailingPE', 'N/A'))
     c4.metric("Beta (Volatilidad)", info.get('beta', 'N/A'))
 
-    # --- FILA 2: MÉTRICAS DE OPERACIÓN ---
+    #MÉTRICAS DE OPERACIÓN
     c5, c6, c7, c8 = st.columns(4)
     c5.metric("EPS (Beneficio/Acción)", f"${info.get('trailingEps', 'N/A')}")
     c6.metric("Volumen Diario", f"{info.get('volume', 0):,}")
     c7.metric("Máx 52 Semanas", f"${info.get('fiftyTwoWeekHigh', 'N/A')}")
     c8.metric("Mín 52 Semanas", f"${info.get('fiftyTwoWeekLow', 'N/A')}")
 
-    # Gráficos
+    #Gráficos
     hist = fin.calcular_indicadores(stock_obj.history(period="2y"))
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.08, 
                        row_heights=[0.7, 0.3], subplot_titles=("Precio y SMA 200", "RSI"))
@@ -79,7 +75,7 @@ if ticker:
     fig.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark", height=600)
     st.plotly_chart(fig, use_container_width=True)
 
-    # IA
+    #IA
     st.divider()
     if st.button("Generar Análisis IA"):
         with st.spinner("Analizando noticias..."):
